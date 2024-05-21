050333fdf77ae207837a60d859aa30739908a633
Valentine <valentine.delevaux@epfl.ch>
2024-05-21 16:08:02 +0200

    asdfg

--- scripts/utils.py
+++ scripts/utils.py
@@ -38,21 +38,30 @@ def plot_chasing(time, fly0_speeds, fly1_speeds, proximities, smooth=True):
     palette = sns.color_palette("Set2", 3)
 
     fig, ax1 = plt.subplots(figsize=(20, 6))
-    line1 = ax1.plot(time, fly0_speeds, color=palette[0], label="Chasing fly speed")
-    line2 = ax1.plot(time, fly1_speeds, color=palette[1], label="Target fly speed")
-    ax1.set_xlabel("time (s)")
-    ax1.set_ylabel("speed (m/s)")
+    line1 = ax1.plot(time, fly0_speeds, color='seagreen', label="Chasing fly speed", linewidth = 3)
+    line2 = ax1.plot(time, fly1_speeds, color='skyblue', label="Target fly speed", linewidth = 3)
+    ax1.set_xlabel("Time (s)", fontsize = 'xx-large')
+    ax1.set_ylabel("Speed (mm/s)", fontsize = 'xx-large')
+
+    ax1.set_xlim(0, 3.5)  # Set x-axis limits
+    ax1.set_ylim(0, 25)
+    
 
     ax2 = ax1.twinx()
-    line3 = ax2.plot(time, proximities, color=palette[2], label="Proximity of the flies")
-    ax2.set_ylabel("Proximity (pixels)")
+    line3 = ax2.plot(time, proximities, color= 'orchid', label="Proximity of the flies", linewidth = 3)
+    ax2.set_ylabel("Proximity (pixels)", fontsize = 'xx-large')
 
+    ax2.set_ylim(0, 0.06)
     # Create a combined legend for all lines
     lines = line1 + line2
     labels = [l.get_label() for l in lines]
-    ax1.legend(lines, labels, loc='upper left')
-    ax2.legend([line3[0]], [line3[0].get_label()], loc='upper right')
+    ax1.legend(lines, labels, loc='upper left', fontsize = 'xx-large')
+    ax2.legend([line3[0]], [line3[0].get_label()], loc='upper right', fontsize = 'xx-large')
 
+    ax1.tick_params(axis='both', which='major', labelsize=15)
+
+    ax2.tick_params(axis='both', which='major', labelsize=15)
+    
     fig.tight_layout()
     plt.show()
 
